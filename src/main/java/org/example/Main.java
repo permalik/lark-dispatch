e0d0f7db6b180fc812188178fd4ae34fa754cd77
permalik <permalik@protonmail.com>
2025-07-30 23:50:29 -0500

    feat: init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        PromptCleanConsumer consumer = new PromptCleanConsumer("prompt.clean");
+        InferenceRequestProducer producer = new InferenceRequestProducer("inference.request");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down..");
+            consumer.close();
+            producer.close();
+        }));
+
+        System.out.printf("Starting Dispatch..");
+
+        try {
+            while (true) {
+                String processedInferenceRequest = consumer.consumeAndProcess();
+                if (processedInferenceRequest != null) {
+                    producer.produce("new_inf_res", processedInferenceRequest);
+                }
+
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException err) {
+            System.err.println("Interrupted: " + err.getMessage());
+        }
+
+    }
+}
